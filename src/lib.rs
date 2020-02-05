4002da53d2f56c60c7aba0c922e30d71e06ee655
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-02-05 23:32:43 +0100

    accept &&mut Storage

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,14 @@ impl<'a, T> Not for &'a Storage<T> {
     }
 }
 
+impl<'a, T> Not for &'a &mut Storage<T> {
+    type Output = NegatedStorage<'a, T>;
+
+    fn not(self) -> NegatedStorage<'a, T> {
+        NegatedStorage(self)
+    }
+}
+
 pub struct NegatedIter<'a, T>(Iter<'a, T>);
 
 impl<'a, T> Iterator for NegatedIter<'a, T> {
@@ -131,6 +139,15 @@ impl<'a, T> Iterator for Iter<'a, T> {
 }
 
 impl<'a, T> Joinable for &'a Storage<T> {
+    type Joined = Iter<'a, T>;
+    type Item = &'a T;
+
+    fn join(self) -> Joined<Self> {
+        Joined::new(Iter(self.inner.iter()), self.inner.len())
+    }
+}
+
+impl<'a, T> Joinable for &'a &mut Storage<T> {
     type Joined = Iter<'a, T>;
     type Item = &'a T;
 
